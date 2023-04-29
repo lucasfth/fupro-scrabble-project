cfa7ee65a2420fbdaa4cb6b16b7211c6e697c2ca
A-Guldborg <aguh@itu.dk>
2023-04-29 18:16:44 +0200

    insert in Trie

--- ScrabbleBot/Trie.fs
+++ ScrabbleBot/Trie.fs
@@ -15,37 +15,14 @@ let insert (root : Trie) (word : string) =
                 let charExists = Map.tryFind fstChar nodeList
                 match charExists with
                 | Some (child) ->
-                    let temp = aux child remaining
-                    Map.add fstChar temp nodeList
+                    Data (isWord, Map.add fstChar (aux child remaining) nodeList)
                 | None -> 
                     // Create subnode
-                    let newNodeList = 
+                    let child = 
                         Map.add fstChar empty nodeList |> 
                         Map.find fstChar
                     
-                    aux newNodeList remaining
-                    // Call aux on subnode
-            | [] -> Data (true, Map.empty) // Set to true and create a leaf
-
-    aux root (Seq.toList word)
-
-let insert (root : Trie) (word : string) = 
-    let rec aux currentNode (subword : List<char>) acc = 
-        match currentNode with
-        | Data (isWord, nodeList) -> 
-            match subword with
-            | fstChar :: remaining ->
-                // Insert fstChar in map if existing
-                let charExists = Map.tryFind fstChar nodeList
-                match charExists with
-                | Some (child) -> aux child remaining 
-                | None -> 
-                    // Create subnode
-                    let newNodeList = 
-                        Map.add fstChar empty nodeList |> 
-                        Map.find fstChar
-                    
-                    aux newNodeList remaining
+                    Data (isWord, Map.add fstChar (aux child remaining) nodeList)
                     // Call aux on subnode
             | [] -> Data (true, Map.empty) // Set to true and create a leaf
 
