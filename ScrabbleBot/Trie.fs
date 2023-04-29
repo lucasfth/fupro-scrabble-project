28ba39f4e6db70281388c76559983543b808bd06
A-Guldborg <aguh@itu.dk>
2023-04-29 19:30:41 +0200

    mkDict + lookup

--- ScrabbleBot/Trie.fs
+++ ScrabbleBot/Trie.fs
@@ -5,8 +5,8 @@ type Trie =
 
 let empty = Data (false, Map.empty)
 
-let insert (root : Trie) (word : string) = 
-    let rec aux currentNode (subword : List<char>) = 
+let insert (word : string) (root : Trie) = 
+    let rec aux (subword : List<char>) currentNode = 
         match currentNode with
         | Data (isWord, nodeList) -> 
             match subword with
@@ -15,26 +15,43 @@ let insert (root : Trie) (word : string) =
                 let charExists = Map.tryFind fstChar nodeList
                 match charExists with
                 | Some (child) ->
-                    Data (isWord, Map.add fstChar (aux child remaining) nodeList)
+                    Data (isWord, Map.add fstChar (aux remaining child) nodeList)
                 | None -> 
-                    // Create subnode
+                    // Create child and then step into it
                     let child = 
                         Map.add fstChar empty nodeList |> 
                         Map.find fstChar
                     
-                    Data (isWord, Map.add fstChar (aux child remaining) nodeList)
+                    Data (isWord, Map.add fstChar (aux remaining child) nodeList)
                     // Call aux on subnode
             | [] -> Data (true, nodeList) // Set to true and create a leaf
 
-    aux root (Seq.toList word)
+    aux (Seq.toList word) root
 
-let step c d : (bool * Trie) =
-    match d with
+let trie = insert "Hello" empty |> insert "Hi" |> insert "Hell"
+
+let step c dict : (bool * Trie) =
+    match dict with
     | Data (isWord, nodes) -> 
         let trie = Map.tryFind c nodes
         match trie with
         | Some (Data (d, a)) -> (d, Data (d, a))
         | None -> (false, empty)
+
+let mkDict (words : string seq) =
+    Seq.fold (fun dict word -> insert word dict) empty words
+
+let lookup w d =
+    let rec aux word dict =
+        match word with
+        | fstChar :: [] -> (fst (step fstChar dict))
+        | fstChar :: remaining -> aux remaining (snd (step fstChar dict))
+        | [] -> failwith "Something went wrong"
+    aux (Seq.toList w) d
+
+let reverse d (b, dict) = failwith "Not implemented"
+
+let isGaddag = fun _ -> false
 
 // Nyt link
 // https://github.com/mrandri19/trie-fsharp/blob/master/simpleTrie/trie.fsx 
