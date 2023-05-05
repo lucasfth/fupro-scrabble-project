1f67ae804f26c62325bf9549d2db6e2dffa1505e
Lucasfth <luha@itu.dk>
2023-05-05 22:01:43 +0200

    Change colors back to default
    The colors were scuffed for a lot of time do to a long lasting
    prank by Guldborg

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -23,8 +23,8 @@ let spawnMultiples name dict bot =
 let main argv =
     ScrabbleUtil.DebugPrint.toggleDebugPrint false // Change to false to supress debug output
 
-    System.Console.BackgroundColor <- System.ConsoleColor.Cyan
-    System.Console.ForegroundColor <- System.ConsoleColor.White
+    System.Console.BackgroundColor <- System.ConsoleColor.White
+    System.Console.ForegroundColor <- System.ConsoleColor.Black
     System.Console.Clear()
 
     // let board = ScrabbleUtil.StandardBoard.standardBoard ()
